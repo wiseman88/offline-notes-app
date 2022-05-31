857d8e9b192a027f68004632a47c29b65f9ad25b
wiseman88 <wiseadam02@gmail.com>
2022-05-31 13:25:54 +0200

    frontend layout created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <div class="flex w-screen h-screen text-gray-700">
+    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+      <!-- sidebar -->
+    </div>
+    <div class="flex flex-col flex-grow">
+      <!-- main content -->
+    </div>
+  </div>
 </template>
 
 <script>
