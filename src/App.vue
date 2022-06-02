094869f54a800667926734d1d54a63f2aa7df5e3
wiseman88 <wiseadam02@gmail.com>
2022-06-02 17:33:35 +0200

    save note refactor

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,25 @@
     <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
       <!-- sidebar -->
       <div class="h-0 overflow-auto flex-grow">
+        <div class="flex items-center h-8 px-3 group mt-4">
+          <div class="flex items-center flex-grow focus:outline-none">
+            <a 
+              href="#"
+              @click.prevent="showAllNotes"
+              class="ml-2 leading-none font-medium text-sm">
+              All Notes
+            </a>
+          </div>
+          <button class="add-note" @click.prevent="addNewNote">
+            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
+              </path>
+            </svg>
+          </button>
+        </div>
         <div class="mt-4">
-          <a 
-            v-for="note in notes" 
-            :key="note.created" 
-            href="#"
-            @click.prevent="openNote(note)" 
+          <a v-for="note in notes" :key="note.created" href="#" @click.prevent="openNote(note)"
             class="flex items-center h-8 text-sm pl-8 pr-3">
             <span class="ml-2 leading-none">
               {{ new Date(note.created).toLocaleString() }}
@@ -23,7 +36,7 @@
         <editor-content :editor="editor" />
       </div>
       <div class="h-16 p-2 bg-gray-100 border-t border-gray-300 text-right">
-        <button class="bg-gray-300 border rounded px-4 py-2" @click="saveNote()">
+        <button class="save-note" @click="saveNote()">
           Save Note
         </button>
       </div>
@@ -125,7 +138,42 @@ export default {
     openNote(note){
       this.editor.commands.setContent(note.content);
       this.activeNote = note;
+    },
+    showAllNotes(){
+      this.editor.commands.clearContent();
+      this.activeNote = {};
+    },
+    addNewNote(){
+      return new Promise((resolve, reject) => {
+        let transaction = this.database.transaction('notes', 'readwrite');
+        transaction.oncomplete = e => {
+          resolve();
+        }
+
+        let now = new Date();
+        let note = {
+          content: '',
+          created: now.getTime()
+        }
+
+        // add same note to the sidebar
+        this.notes.unshift(note);
+
+        // set activeNote as the new note
+        this.activeNote = note;
+        transaction.objectStore('notes').add(note);
+      });
     }
   }
 }
 </script>
+
+<style lang="postcss">
+button.save-note {
+  @apply bg-none border border-gray-900 rounded py-1 px-4 mr-4 mt-3 hover:bg-gray-900 hover:text-white;
+}
+
+button.add-note {
+  @apply flex items-center justify-center h-6 w-6 ml-1 rounded hover:bg-gray-300;
+}
+</style>
