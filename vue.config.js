178a574f67a189cb4137284c18f8bc6b0e72c61d
wiseman88 <wiseadam02@gmail.com>
2022-06-03 16:34:22 +0200

    service-worker.js created

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
         pwa: {
+                workboxPluginMode: "InjectManifest",
                 workboxOptions: {
-                        skipWaiting: true
+                        swSrc: "./src/service-worker.js"
                 }
         }
 }
